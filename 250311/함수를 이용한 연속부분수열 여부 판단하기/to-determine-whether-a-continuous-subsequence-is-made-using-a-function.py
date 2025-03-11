eb8b46b8a1314fe8194bbbe5a6e3b424da85de6f
Danjjinbibbang <112943173+Danjjinbibbang@users.noreply.github.com>
2025-03-11 18:04:04 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250311/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,52 @@
+n1, n2 = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+# Please write your code here.
+# n1 > n2
+# n1에 대해서 n2를 탐색하면서 n1의 원소와 n2의 원소가 같다면 다음 원소 비교
+# 연달아서 같다면 Yes 출력
+
+def search_list(a, b, n1, n2):
+    if(n2 == 1):
+        for i in range(n1):
+            if(a[i] == b[0]):
+                return True
+    else:
+        flag = False
+        for i in range(len(a)):
+            for j in range(len(b)):
+                if(a[i] != b[j]):
+                    flag = False
+                else:
+                    flag = True
+                if(a[i] == b[j] and i < n1-1 and j < n2-1):
+                    i += 1
+                    j += 1
+                    if(a[i] != b[j]):
+                        return False
+        return flag
+
+def search_list1(a, b, n1):
+    for i in range(n1):
+        if(a[i] != b[i]):
+            return False
+    return True
+
+
+if(n1 == n2):
+    if(search_list1(a, b, n1)):
+        print('Yes')
+    else:
+        print('No')
+
+
+if(n1 > n2):
+    if(search_list(a, b, n1, n2)):
+        print('Yes')
+    else:
+        print('No')
+
+if(n1 < n2):
+    print('No')
+
