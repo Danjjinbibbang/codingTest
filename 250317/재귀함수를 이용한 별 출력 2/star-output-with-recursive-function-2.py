6b5b97c708cebd4c233e03a19aafd3670fe77e26
Danjjinbibbang <112943173+Danjjinbibbang@users.noreply.github.com>
2025-03-17 17:03:54 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 57ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250317/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+# Please write your code here.
+def print_star(n):
+    if(n == 0):
+        return
+    for i in range(n):
+        print("*", end=" ")
+    print()
+    print_star(n-1)
+    for i in range(n):
+        print("*", end=" ")
+    print()
+
+print_star(n)
