5a9beb8ae3aea2be6d513c1506ddfe0125661261
Danjjinbibbang <112943173+Danjjinbibbang@users.noreply.github.com>
2025-03-18 11:19:28 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250318/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 250318/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -2,7 +2,7 @@ n = int(input())
 arr = list(map(int, input().split()))
 # Please write your code here.
 def print_max(n, arr):
-    if(n == 1):
+    if(n == 0):
         return arr[n]
     return max(print_max(n-1, arr), arr[n-1])
 print(print_max(n, arr))
